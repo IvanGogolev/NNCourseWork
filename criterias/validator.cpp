19af3a1c8fb5087eccc64e6aec3336406effeb11
Ivan Gogolev <gogolev-04@bk.ru>
2026-03-05 11:36:23 +0400

    final criterias

---
+++ criterias/validator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "testlib.h"
+using namespace std;
+int main(int argc, char *argv[]) {
+    registerValidation(argc, argv);
+    inf.readInt(0, 1000000000, "a");
+    inf.readSpace();
+    inf.readInt(0, 1000000000, "b");
+    inf.readEoln();
+    inf.readEof();
+}
